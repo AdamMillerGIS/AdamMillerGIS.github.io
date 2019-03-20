a5a26f3b5c056253797bd6d76ec148d50fe94fb8
Adam Miller <armiller34@wisc.edu>
2019-03-19 21:46:09 -0500

    Loaded topojson
    Loaded topojson and converted it back to geojson

--- d3-coordinated-viz/js/main.js
+++ d3-coordinated-viz/js/main.js
@@ -4,16 +4,19 @@ window.onload = setMap();
 //set up choropleth map
 function setMap(){
     //use queue to parallelize asynchronous data loading
-    d3.queue()
+    d3_queue.queue()
         .defer(d3.csv, "data/NSAAChamps.csv") //load attributes from csv
         .defer(d3.json, "data/counties.topojson") //load choropleth spatial data
         .await(callback);
 
-        function callback(error, csvData, counties){
-            console.log(error);
-            console.log(csvData);
-            console.log(counties);
-        };
+  function callback(error, csvData, counties){
+      //translate europe TopoJSON
+      var nebraskaCounties = topojson.feature(counties, counties.objects.counties).features;
+
+
+      //examine the results
+      console.log(nebraskaCounties);
+  };
 
 
         //Example 1.4 line 10
